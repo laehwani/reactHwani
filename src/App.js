083cf0d6fc7e5857148220b81e59343731ea54a5
hwani <klh481@naver.com>
2023-10-31 16:21:23 +0900

    간단한 {}, style 주기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-function BigImage() {
-  return <h1>큰 이미지!</h1>;
-}
-
-function SmallImage() {
-  return <h1>작은 이미지!</h1>;
-}
-
 function App() {
-  const number = Math.ceil(Math.random() * 100);
-  const isLarge = number > 50;
+  const myStyle = {
+    color: "blue",
+    backgroundColor: "gold",
+    fontSize: "70px",
+    textAlign: "center",
+  };
   return (
-    <div>
-      <h1>난수 : {number}</h1>
-      <h1>{number > 50 ? "큰 수" : "작은 수"}</h1>
-      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
-      <div>
-        {isLarge && <h1>큰 수임!!</h1>}
-        {isLarge || <h1>작은 수임!!</h1>}
-      </div>
-    </div>
+    <>
+      {/* style 속성은 객체로 주어야 함.
+       */}
+      <div style={myStyle}>Lorem ipsum dolor.</div>
+    </>
   );
 }
-
 export default App;
