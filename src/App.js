0dbcc04778227780a6aa6c5af18ad4efd09989bf
hwani <klh481@naver.com>
2023-10-31 15:37:28 +0900

    간단한 {}, 연산자 활용!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+function MyGame() {
+  return;
+}
 
 function App() {
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+  let message;
+  if (num1 === num2) {
+    message = <h1>당첨</h1>;
+  } else {
+    message = <h1>다시 던져보세요!</h1>;
+  }
   return (
     <div>
-
+      <h1>1번 주사위 : {num1}</h1>
+      <h1>2번 주사위 : {num2}</h1>
+      <h1>{num1 === num2 ? "당첨" : "다시 던져 보세요"}</h1>
+      <MyGame />
+      <hr />
+      {message}
     </div>
   );
 }
