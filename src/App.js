8413a35253fe9d2a2b78d6bfcff0427dcf9a6dba
hwani <klh481@naver.com>
2023-10-31 12:15:31 +0900

    component 연습,작성완료!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
 
+function MyComponent1() {
+  return (
+      <>
+        <p>lorem ipsum dolor</p>
+        <ul>
+          <li>lorem.</li>
+          <li>labore.</li>
+          <li>Quas.</li>
+        </ul>
+      </>
+  );
+}
+function Mycomponent2() {
+  return (
+      <div>
+        <h1>Hello React</h1>
+      </div>
+  )
+}
+
 function App() {
   return (
     <div>
+      <MyComponent1/>
+      {/*<div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
 
+      <Mycomponent2/>
+      {/*
+      <p>lorem</p>
+      <ul>
+        <li>lorem</li>
+        <li>Component</li>
+        <li>lorem Component</li>
+      </ul>
+      */}
     </div>
   );
 }
