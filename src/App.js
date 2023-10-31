e2c01e3839e639e0d08702468c0d0c88937342f0
hwani <klh481@naver.com>
2023-10-31 14:25:24 +0900

    컴포넌트 여러번 사용하기.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-
 function App() {
   return (
     <div>
+      <MyComponent1 />
+      <MyComponent1 />
+      <MyComponent1 />
+      {/*
 
+      */}
+    </div>
+  );
+}
+
+function MyComponent1() {
+  return (
+    <div style={{ width: "100%" }}>
+      <img
+        style={{ width: "100%" }}
+        src="https://i.imgur.com/SuL4x4v.jpeg"
+        alt="경복궁"
+      />
     </div>
   );
 }
