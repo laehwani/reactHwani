bc746a824cc0f92e891160a18d9e8583f427420f
hwani <klh481@naver.com>
2023-10-31 14:15:27 +0900

    prettier 라이브러리추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-
 function App() {
+  // jsx 에서 태그 사용시 꼭 종료태그를 작성해야 함.
+  // 빈 요소이면 시작태그에서 종료
   return (
     <div>
       <h1>hello react</h1>
       <h2>hi react</h2>
-      <MyComp/>
+      <MyComp />
     </div>
   );
 }
 
 function MyComp() {
-  return <h1>HI COMPONENT</h1>
-
+  return <h1>HI COMPONENT</h1>;
 }
 
 export default App;
