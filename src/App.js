12048a21a08f701d9950d2e3794e7db87cf29083
hwani <klh481@naver.com>
2023-10-31 14:42:39 +0900

    변수 사용하기

--- src/App.js
+++ src/App.js
@@ -1,3 +1,23 @@
+function MyComp2() {
+  let name = "화니";
+  let age = 33;
+  return (
+    <div>
+      <h1>Hwani React</h1>
+      <h2>드개재~</h2>
+      <p>
+        Hello {name}
+        <br />
+        {age} 입니다!
+        <br />
+        {age * 2}double 스코어!
+        <br />
+        {name + "선수입니다!"}
+      </p>
+    </div>
+  );
+}
+
 function App() {
   // jsx 에서 태그 사용시 꼭 종료태그를 작성해야 함.
   // 빈 요소이면 시작태그에서 종료
@@ -6,6 +26,7 @@ function App() {
       <h1>hello react</h1>
       <h2>hi react</h2>
       <MyComp />
+      <MyComp2 />
     </div>
   );
 }
