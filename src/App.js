d49d818077e7dbf518aee4c72e4d3e3cee40d99b
hwani <klh481@naver.com>
2023-10-31 11:40:21 +0900

    리액트 첫 커밋!

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@
 function App() {
   return (
     <div>
-
+      <h1>hello react</h1>
+      <h2>hi react</h2>
+      <MyComp/>
     </div>
   );
+}
+
+function MyComp() {
+  return <h1>HI COMPONENT</h1>
+
 }
 
 export default App;
