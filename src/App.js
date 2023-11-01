6e8133814ead601ceacbacc3bfaad5f39e1aeb46
hwani <klh481@naver.com>
2023-11-01 15:48:13 +0900

    react 보통의 css파일 사용시 클래스명이 중복될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { MyComp } from "./Comp/MyComp";
 import { MyBox } from "./Comp/MyBox";
 import "../src/style/style1.css";
+import { MyContainer } from "./Comp/MyContainer";
 
 function App() {
   return (
     <>
       <MyComp />
       <MyBox />
+      <MyContainer />
     </>
   );
 }
