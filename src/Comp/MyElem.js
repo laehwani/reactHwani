ceb9926db6e3bee89d46250fd4ab5aa3ad4d6406
hwani <klh481@naver.com>
2023-11-01 11:48:14 +0900

    react Comp를 다른 파일에 작성후 export, import 하기

---
+++ src/Comp/MyElem.js
@@ -0,0 +1,3 @@
+export function MyElem() {
+  return null;
+}
---
+++ src/Comp/MyHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function MyHeader() {
+  return (
+    <div>
+      <h1>너무 긴 코드가 작성됨...</h1>
+    </div>
+  );
+}
