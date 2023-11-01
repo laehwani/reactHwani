11218b94e88ddfb4d564cc6dd616486016fe19bc
hwani <klh481@naver.com>
2023-11-01 11:47:55 +0900

    react Comp를 다른 파일에 작성후 export, import 하기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
-import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import "bootstrap/dist/css/bootstrap.css";
-import Button from "react-bootstrap/Button";
-import Nav from "react-bootstrap/Nav";
-import shoes from "./img/shoes.jpg";
-import * as PropTypes from "prop-types";
-
-function MyComp(style, text) {
-  return (
-    <div>
-      <p>{text}</p>
-      <p>{text}</p>
-    </div>
-  );
-}
-
-MyComp.propTypes = {
-  address: PropTypes.string,
-  value: PropTypes.string,
-};
+import React from "react";
+import { MyHeader } from "./Comp/MyHeader";
+import { MyComp } from "./Comp/MyComp";
+import { MyElem } from "./Comp/MyElem";
 
 function App() {
   return (
     <div>
+      <MyHeader />
       <MyComp />
+      <MyComp />
+      <MyElem />
     </div>
   );
 }
