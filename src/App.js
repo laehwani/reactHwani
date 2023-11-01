96fc4b2ded84f0659b8faf92586f2c990abd0e9a
hwani <klh481@naver.com>
2023-11-01 15:38:41 +0900

    react 보통의 css파일 사용하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import { MyText } from "./component/MyText";
+import "../src/style/mystyle.css";
 function App() {
   return (
     <div>
-
+      <MyText />
     </div>
   );
 }
---
+++ src/component/MyText.js
@@ -0,0 +1,12 @@
+export function MyText() {
+  return (
+    <div>
+      <h1 className="error">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis
+        doloribus enim expedita id incidunt provident quibusdam recusandae sequi
+        sit voluptates. Aliquam, assumenda culpa eum illum minima natus
+        perferendis saepe voluptatem?
+      </h1>
+    </div>
+  );
+}
