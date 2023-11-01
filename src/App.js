6e13eaa5f6e2fd4a2cbe4098edd2c6298989cbcb
hwani <klh481@naver.com>
2023-11-01 14:27:40 +0900

    react Comp를 다른 파일에 작성후 export, import 하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import MyBox, { team, person } from "./MyBox";
 
 function App() {
   return (
     <div>
-
+      <MyBox />
+      {/*<MyContainer/>*/}
+      <h1>{person}</h1>
+      <h1>{team}</h1>
     </div>
   );
 }
---
+++ src/MyBox.js
@@ -0,0 +1,7 @@
+function MyBox() {
+  return <div></div>;
+}
+export const person = { name: "화니", address: "서울" };
+export const team = { title: "유후", score: 3 };
+
+export default MyBox;
