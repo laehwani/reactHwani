1c742876a8e07d0b5ff2316abd9688baab49b6da
hwani <klh481@naver.com>
2023-11-01 12:35:08 +0900

    react Comp를 다른 파일에 작성후 export, import 하기

---
+++ src/MyElem.js
@@ -0,0 +1,14 @@
+export function MyElem() {
+  let food = "뜨끈한거";
+  return <div>{food}</div>;
+}
+export function MyNavbar() {
+  return <div>{country}</div>;
+}
+
+export function MyContent() {
+  return <div>{text}</div>;
+}
+export const country = "서울";
+export let text = "취업하고싶어요";
+export let value = "화니는 배고파요";
