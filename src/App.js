a4cbee365344ff31ac2abea12957768b1c2aea22
hwani <klh481@naver.com>
2023-11-01 11:16:48 +0900

    react props 사용법1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import * as PropTypes from "prop-types";
+
+function MyElem({ text, style }) {
+  style.textAlign = "center";
+  return (
+    <div style={style}>
+      <p>{text}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div>
-
+    <div
+      style={{
+        padding: "10px",
+        margin: "auto",
+        borderRadius: "10px",
+        border: "1px solid black",
+        backgroundColor: "coral",
+      }}
+    >
+      <MyElem
+        style={{ color: "skyblue", backgroundColor: "brown" }}
+        text="오늘 점심 뭐먹지?"
+      />
+      <MyElem
+        style={{ color: "yellow", backgroundColor: "green" }}
+        text="비가 오니 삭신이 쑤셔"
+      />
     </div>
   );
 }
