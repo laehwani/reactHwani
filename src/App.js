5afa28a196c4177fd6cecfc1ff2ec260addb58e0
hwani <klh481@naver.com>
2023-11-02 10:18:19 +0900

    chakraui Box컴포넌트 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./style/style1.module.css";
-
+import { Box } from "@chakra-ui/react";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Box bgColor={"tomato"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"red.700"}>Lorem ipsum dolor.</Box>
+      <Box bgColor={"blue.700"}>Nihil, numquam quibusdam?</Box>
+      <Box bgColor={"orange"}>Cum, maiores quis!</Box>
+      <Box bgColor={"yellow"}>Delectus, necessitatibus, perspiciatis?</Box>
+      <Box>Facere, fugit, numquam?</Box>
+      <Box>Aut, enim, similique.</Box>
+      <Box>Blanditiis, distinctio, voluptates.</Box>
+      <Box>Labore perferendis, porro.</Box>
+      <Box>Accusamus assumenda, ratione.</Box>
+      <Box>Blanditiis, quaerat soluta!</Box>
+    </>
+  );
 }
 
 export default App;
