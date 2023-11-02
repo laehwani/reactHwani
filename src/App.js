2d5cfccad935f77f3ec1e3b7c19bf97a96649c21
hwani <klh481@naver.com>
2023-11-02 10:23:44 +0900

    master 에 필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
-import { Box } from "@chakra-ui/react";
 function App() {
-  return (
-    <>
-      <Box bgColor={"tomato"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"red.700"}>Lorem ipsum dolor.</Box>
-      <Box bgColor={"blue.700"}>Nihil, numquam quibusdam?</Box>
-      <Box bgColor={"orange"}>Cum, maiores quis!</Box>
-      <Box bgColor={"yellow"}>Delectus, necessitatibus, perspiciatis?</Box>
-      <Box>Facere, fugit, numquam?</Box>
-      <Box>Aut, enim, similique.</Box>
-      <Box>Blanditiis, distinctio, voluptates.</Box>
-      <Box>Labore perferendis, porro.</Box>
-      <Box>Accusamus assumenda, ratione.</Box>
-      <Box>Blanditiis, quaerat soluta!</Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
