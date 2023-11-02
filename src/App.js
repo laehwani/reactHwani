f0da7f138dae26373d10c1eed6ddcaae5656ca77
hwani <klh481@naver.com>
2023-11-02 14:13:00 +0900

    chakra Modal컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from "react";
-import { Box, Center, Circle } from "@chakra-ui/react";
-import { PhoneIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function MdodalCloseButton() {
+  return null;
+}
 
 function App() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <div>
-      <Center bg={"red"} h={"200px"}>
-        <Box w={"100px"} h={"100px"} bg={"blue"}>
-          Lorem ipsum.
-        </Box>
-      </Center>
-
-      <Center bg={"red.200"} h={"200px"}>
-        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
-          <PhoneIcon />
-        </Circle>
-      </Center>
+      <Button onClick={onOpen}>모달 열기</Button>
+      <Modal
+        closeOnOverlayClick={false}
+        isCentered
+        isOpen={isOpen}
+        onClose={onClose}
+        motionPreset="slideInTop"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>모달의 제목</ModalHeader>
+          <MdodalCloseButton />
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
+            accusantium, aspernatur aut blanditiis consequatur dolorem eos, et
+            explicabo illum itaque maxime nesciunt non nostrum odio pariatur
+            placeat soluta tempora unde?
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" onClick={onClose}>
+              버튼닫기
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </div>
   );
 }
