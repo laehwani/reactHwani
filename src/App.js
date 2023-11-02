6b40eb5e76d62cb5e27f044790c450047ec3088c
hwani <klh481@naver.com>
2023-11-02 12:37:42 +0900

    chakra Center와 Icon 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,65 +1,21 @@
-import {
-  Box,
-  SimpleGrid,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-} from "@chakra-ui/react";
 import React from "react";
+import { Box, Center, Circle } from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
     <div>
-      <SimpleGrid columns={1} gap={5}>
-        <Box>
-          {/*Table>Thead>Tr>Th*4>lorem1^^Tbody>Td*4>lorem1*/}
-          <Table variant={"striped"} colorSchema={"red"} size={"sm"}>
-            <Thead>
-              <Tr>
-                <Th>Lorem ipsum.</Th>
-                <Th>Explicabo, nam.</Th>
-                <Th>Eaque, reprehenderit.</Th>
-                <Th>Fuga, omnis.</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td>Lorem ipsum.</Td>
-                <Td>Adipisci, dolore.</Td>
-                <Td>A, ut?</Td>
-                <Td>Ipsa, vero.</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem ipsum.</Td>
-                <Td>Dicta, praesentium.</Td>
-                <Td>Explicabo, recusandae.</Td>
-                <Td>Expedita, illo!</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem ipsum.</Td>
-                <Td>Blanditiis, voluptatem.</Td>
-                <Td>At, quam!</Td>
-                <Td>Suscipit, ut?</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem ipsum.</Td>
-                <Td>Illum, sit!</Td>
-                <Td>Beatae, cupiditate.</Td>
-                <Td>Quos, reiciendis!</Td>
-              </Tr>
-              <Tr>
-                <Td>Lorem ipsum.</Td>
-                <Td>Eius, neque.</Td>
-                <Td>Veritatis, voluptatum?</Td>
-                <Td>Est, minus!</Td>
-              </Tr>
-            </Tbody>
-          </Table>
+      <Center bg={"red"} h={"200px"}>
+        <Box w={"100px"} h={"100px"} bg={"blue"}>
+          Lorem ipsum.
         </Box>
-      </SimpleGrid>
+      </Center>
+
+      <Center bg={"red.200"} h={"200px"}>
+        <Circle bg={"blue.200"} w={"100px"} h={"100px"}>
+          <PhoneIcon />
+        </Circle>
+      </Center>
     </div>
   );
 }
