8f32ea496b17d86fc4fc586bcfdef73957ede0ef
hwani <klh481@naver.com>
2023-11-02 14:24:38 +0900

    chakra Toast 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,48 +1,24 @@
 import React from "react";
-import {
-  Button,
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
-  useDisclosure,
-} from "@chakra-ui/react";
-
-function MdodalCloseButton() {
-  return null;
-}
+import { Button, useToast } from "@chakra-ui/react";
 
 function App() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
   return (
     <div>
-      <Button onClick={onOpen}>모달 열기</Button>
-      <Modal
-        closeOnOverlayClick={false}
-        isCentered
-        isOpen={isOpen}
-        onClose={onClose}
-        motionPreset="slideInTop"
+      <Button
+        onClick={() =>
+          toast({
+            title: "저장 완료",
+            description: "게시물이 저장되었습니다.",
+            duration: 2000,
+            position: "top-right",
+            isClosable: true,
+            status: "error",
+          })
+        }
       >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>모달의 제목</ModalHeader>
-          <MdodalCloseButton />
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A
-            accusantium, aspernatur aut blanditiis consequatur dolorem eos, et
-            explicabo illum itaque maxime nesciunt non nostrum odio pariatur
-            placeat soluta tempora unde?
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" onClick={onClose}>
-              버튼닫기
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+        토스트 띄우기
+      </Button>
     </div>
   );
 }
