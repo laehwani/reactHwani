2a1e7bc2242542734c8cf4a3c2a5a80fface6ec9
hwani <klh481@naver.com>
2023-11-03 10:28:15 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Button } from "@chakra-ui/react";
+
+function MyButton({ executeClick, children }) {
+  return (
+    <div>
+      <Button onClick={{ executeClick }}>{children}</Button>
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  console.log("hello");
+  return (
+    <>
+      <MyButton executeClick={"hello"}></MyButton>
+      <MyButton executeClick={"greetiong"}></MyButton>
+    </>
+  );
 }
 
 export default App;
