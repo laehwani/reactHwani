8afbff4c6d59f197f01904dae20a0e50d3681d7c
hwani <klh481@naver.com>
2023-11-03 16:46:11 +0900

    react 간단 게시판 예제

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
 import React, { useState } from "react";
-import { Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [submitting, setSubmitting] = useState(false);
 
-  function handleChange(e) {
-    setMessage(e.target.value);
+  function handleNameInput(e) {
+    setName(e.target.value);
+  }
+  function handlePasswordInput(e) {
+    setPassword(e.target.value);
+  }
+  function handleEmailInput(e) {
+    setEmail(e.target.value);
+  }
+  function handleSubmit(e) {
+    setSubmitting(e.target.value);
+    // axios
+    //   .post("/adduser", {
+    //     name,
+    //     email,
+    //     password,
+    //   })
+    //   .then((response) => console.log("성공할때 해야하는일"))
+    //   .catch((error) => console.log("실패"))
+    //   .finally(() => console.log("꼭 해야하는일"));
   }
   return (
-    <div>
-      <Input type="text" value={message} onChange={handleChange} />
-    </div>
+    <Center>
+      <Box w={"480px"}>
+        <FormControl>
+          <FormLabel>Name</FormLabel>
+          <Input type="text" value={name} onChange={handleNameInput}></Input>
+          <FormHelperText>띄어쓰기 없이 입력해주세요.</FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel>password</FormLabel>
+          <Input
+            type="password"
+            value={password}
+            onChage={handlePasswordInput}
+          ></Input>
+          <FormHelperText>
+            특수기호와 숫자를 하나 이상 입력해주세요.
+          </FormHelperText>
+        </FormControl>
+        <FormControl>
+          <FormLabel>UserEmail</FormLabel>
+          <Input type="email" value={email} onChage={handleEmailInput}></Input>
+          <FormHelperText>이메일은 중복될 수 없습니다..</FormHelperText>
+        </FormControl>
+        <Button onChange={handleSubmit} colorScheme="whatsapp">
+          확인
+        </Button>
+      </Box>
+    </Center>
   );
 }
 
