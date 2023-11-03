e3b6edbd2b1113509c5eee65c05b3ea7b0b54218
hwani <klh481@naver.com>
2023-11-03 14:25:47 +0900

    react event-bubbling 과 stoppropagation 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { Button, Center } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  function handleOuterBoxClick() {
+    console.log("바깥 상자 클릭함");
+  }
+  function handleInnerBoxClick(e) {
+    e.stopPropagation();
+    console.log("안쪽 상자 클릭함");
+  }
+  function handleBtnClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭함");
+  }
+  return (
+    <>
+      <Center
+        onClick={handleOuterBoxClick}
+        h={"200px"}
+        w={"200px"}
+        bg={"skyblue"}
+      >
+        <Center
+          onClick={handleInnerBoxClick}
+          h={"100px"}
+          w={"100px"}
+          bg={"coral"}
+        >
+          <Button onClick={handleBtnClick} bg={"peachpuff"}>
+            버튼
+          </Button>
+        </Center>
+      </Center>
+    </>
+  );
 }
 
 export default App;
