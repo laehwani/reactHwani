e567f9507f458620a2f91f7ccd4194fbdee4a5ce
hwani <klh481@naver.com>
2023-11-03 11:40:44 +0900

    react 이벤트 핸들러 메소드에 event 객체가 전달된다.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 
+import React from "react";
 import { Button } from "@chakra-ui/react";
 
-// 함수명 작성 관습
-// handle 이벤트명
-function App() {
-  function handleClickBtn1() {
-    console.log("버튼버튼");
+function App(props) {
+  // 이벤트 핸들러 메소드
+  // event 객체를 매개값으로 넣어줌
+  function handleClick(event) {
+    // console.log(event);
+    console.log(event.target);
+    // console.log(event.target.className);
+    // console.log(event.target.type);
   }
-
-  function handleClickBtn2() {
-    console.log("두번째버튼버튼");
-  }
-
-  function handleButtonClick(number) {
-    console.log(number + "번째 버튼 클릭됨");
-  }
-
   return (
     <div>
-      <Button onClick={handleClickBtn1}>첫번째버튼</Button>
-      <Button onClick={handleClickBtn2}>두번쨰버튼</Button>
-      <Button onClick={() => handleButtonClick(3)}>세번쨰버튼</Button>
-      <Button onClick={() => handleButtonClick(4)}>네번쨰버튼</Button>
+      <Button colorScheme="orange" onClick={handleClick}>
+        버튼1
+      </Button>
+      <Button colorScheme="blue" onClick={handleClick}>
+        버튼2
+      </Button>
     </div>
   );
 }
+
 export default App;
