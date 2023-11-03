381d6c570293a1c0ec18956860e8512de8848019
hwani <klh481@naver.com>
2023-11-03 09:36:13 +0900

    react children 및 버튼에 링크걸기 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import React from "react";
+import * as PropTypes from "prop-types";
+import { Button, Stack } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <div>
+      <Button as="a" href={to} colorScheme="teal" variant="outline">
+        {children}
+      </Button>
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to={"https://www.naver.com"}>naver 로 </MyLink>
+      <MyLink to={"https://www.daum.net"}>daum 으로</MyLink>
+    </>
+  );
 }
 
 export default App;
