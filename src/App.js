656ffbef3ccfbd5045a1464876f2241f2b2f635f
hwani <klh481@naver.com>
2023-11-03 15:08:51 +0900

    react usestate 중요

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React, { useState } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  // 상태(state) 가 변경(이전 state 와 달라짐) 되면
-  // Component 를 다시 그린다. 이것을 're render' 라고 한다.
-  // state : react 가 관리하는 component 의 값.
-
-  // state 를 얻는 방법
-  // UseState();
-  // UseState 는 배열을 리턴한다.
-  // index 0 : 현재상태(state),
-
-  // const [message, setMessage] = useState("");
-  const [message, setMessage] = useState("");
-  let text = "초기 메세지";
-
-  function handleInputChange(e) {
-    text = e.target.value;
-    console.log("text", text);
-  }
-  function handleBtnClick(e) {
-    setMessage("바뀐 상태");
+  let [number, setNumber] = useState(0);
+  function handleClick() {
+    setNumber(number + 1);
   }
   return (
     <div>
-      <Input onChange={handleInputChange}></Input>
-      <p>{text}</p>
-      <Button onClick={handleBtnClick}>상태 바꾸기</Button>
-      <Button onClick={() => setMessage("")}>빈 문자열로 바꾸기</Button>
-      <p>{message}</p>
+      <Button onClick={handleClick}>바꾸기</Button>
+      <Text>{number}</Text>
     </div>
   );
 }
