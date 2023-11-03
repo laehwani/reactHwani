53b574161ddfae850ed969e9c02e84476c3b207d
hwani <klh481@naver.com>
2023-11-03 12:10:44 +0900

    react 이벤트타겟의 값이 변경되어도 바로 dom에 반영되지는 않는다

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
-
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  // 이벤트 핸들러 메소드
-  // event 객체를 매개값으로 넣어줌
-  function handleClick(event) {
-    // console.log(event);
-    console.log(event.target);
-    // console.log(event.target.className);
-    // console.log(event.target.type);
+  let text = "initial text";
+  function handleChange(e) {
+    // console.log("인풋값이 변경됨");
+    console.log(e.target.value);
+    text = e.target.value;
+    console.log("text", text);
   }
   return (
     <div>
-      <Button colorScheme="orange" onClick={handleClick}>
-        버튼1
-      </Button>
-      <Button colorScheme="blue" onClick={handleClick}>
-        버튼2
-      </Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }
