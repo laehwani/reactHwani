d230ee07e070323b69bf3c117030decc4c60ec11
hwani <klh481@naver.com>
2023-11-03 16:12:49 +0900

    react usestate 중요

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
+
+function App(props) {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+
+  function InputChange(e) {
+    setNumber1(Number(e.target.value));
+  }
+  function InputPlus(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  let result = number1 + number2;
+  return (
+    <div>
+      <Input type="number" onChange={InputChange}></Input> +
+      <Input type="number" onChange={InputPlus}></Input>={result}
+    </div>
+  );
 }
 
 export default App;
