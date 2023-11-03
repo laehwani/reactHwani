4c1e2e676f7548649d461345290c5e66f02430f4
hwani <klh481@naver.com>
2023-11-03 16:24:34 +0900

    react input 요소에 value, onchange prop 사용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  const isOdd = number % 2 == 1;
-  function handleInputChange(e) {
-    setNumber(Number(e.target.value));
+  const [message, setMessage] = useState("");
+
+  function handleChange(e) {
+    setMessage(e.target.value);
   }
   return (
     <div>
-      <Input type="number" onChange={handleInputChange}></Input>
-      <Text>
-        {isOdd && "홀수"}
-        {isOdd || "짝수"}
-      </Text>
+      <Input type="text" value={message} onChange={handleChange} />
     </div>
   );
 }
