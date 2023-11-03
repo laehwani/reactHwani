077c118350177499d94a1c1d40f247c8bb2da165
hwani <klh481@naver.com>
2023-11-03 09:21:59 +0900

    react children 연습

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Col, Form, Nav, Row } from "react-bootstrap";
-import { Button } from "@chakra-ui/react";
-import { SearchBar } from "./SearchBar";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+function MyComp({ text, address, children }) {
+  return (
+    <div>
+      <p>{text}</p>
+      <p>{address}</p>
+      <p>{children}</p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item xs={8} md={8}>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-      <SearchBar />
-    </Nav>
+    <div>
+      <MyComp text={"hello"} address={"seoul"}></MyComp>
+    </div>
   );
 }
 
