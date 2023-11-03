6eb1a7157668296f84f9ae458efc9c2af848afab
hwani <klh481@naver.com>
2023-11-03 10:49:30 +0900

    react 에로우펑션(화살함수) 중요중요

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  let firstB = () => {
+    console.log("first");
+  };
+  return (
+    <>
+      {/*첫번째 버튼 클릭시 "first" 콘솔출력*/}
+      <Button onClick={firstB}>Lorem.</Button>
+      {/*두번째는 "second" 콘솔출력*/}
+      <Button
+        onClick={() => {
+          console.log("second");
+        }}
+      >
+        Sunt.?
+      </Button>
+    </>
+  );
 }
 
 export default App;
