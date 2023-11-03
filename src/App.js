84fc1b4b6f8c67bed5a1fc9d9e0f851a27f10dc6
hwani <klh481@naver.com>
2023-11-03 10:44:06 +0900

    react 함수와 화살함수

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 
-function MyComp({ text, address, children }) {
+import { Button } from "@chakra-ui/react";
+
+function MyComp({ children, executeClick }) {
   return (
     <div>
-      <p>{text}</p>
-      <p>{address}</p>
-      <p>{children}</p>
+      <Button onClick={executeClick}>{children}</Button>
     </div>
   );
 }
 
 function App() {
+  function func1() {
+    console.log("안녕");
+  }
+  let func2 = () => {
+    console.log("안녀엉");
+  };
+
   return (
     <div>
-      <MyComp text={"hello"} address={"seoul"}></MyComp>
+      <MyComp executeClick={func1}>Button1</MyComp>
+      <MyComp executeClick={func2}>Button2</MyComp>
+      <MyComp
+        executeClick={() => {
+          console.log("펑션안녀엉");
+        }}
+      >
+        Button3
+      </MyComp>
+      <MyComp
+        executeClick={() => {
+          console.log("펑션안녀어어어엉");
+        }}
+      >
+        Button4
+      </MyComp>
     </div>
   );
 }
