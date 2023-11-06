7199334e46202571e585719ff7c8ef66dbc7827e
hwani <klh481@naver.com>
2023-11-06 15:47:12 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App(props) {
-  return <div></div>;
+  const [person, setPerson] = useState({ name: "son", city: "seoul" });
+  return (
+    <div>
+      <Input value={person.name} onChange={handlenamechange}></Input>
+      <Input value={person.name} onChange={handlenamechange}></Input>
+    </div>
+  );
 }
 
 export default App;
