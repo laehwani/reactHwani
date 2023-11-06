33aab4a3547cdf3844627f82f87c109b2a13b9db
hwani <klh481@naver.com>
2023-11-06 16:15:02 +0900

    react 복습필요

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+import { useImmer } from "use-immer";
 
 function App(props) {
   const [person, setPerson] = useState({ name: "son", city: "seoul" });
+  const [user, updateUser] = useImmer({ name: "lee", city: "jeju" });
+
+  function handleNameChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.name = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handleCityChange(e) {
+    const nextPerson = { ...person };
+    nextPerson.city = e.target.value;
+
+    setPerson(nextPerson);
+  }
+
+  function handleUserNameChange(e) {
+    updateUser((draft) => {
+      draft.name = e.target.value;
+    });
+  }
+
+  function handleUserCityChange(e) {
+    updateUser((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
   return (
     <div>
-      <Input value={person.name} onChange={handlenamechange}></Input>
-      <Input value={person.name} onChange={handlenamechange}></Input>
+      <Input value={person.name} onChange={handleNameChange} />
+      <Input value={person.city} onChange={handleCityChange} />
+      <Text color={"red"}>
+        {person.name}은 {person.city}에 삽니다.
+      </Text>
+
+      <hr />
+
+      <Input value={user.name} onChange={handleUserNameChange} />
+      <Input value={user.city} onChange={handleUserCityChange} />
+      <Text>
+        {user.name}은 {user.city}에 삽니다.
+      </Text>
+      <Box></Box>
     </div>
   );
 }
