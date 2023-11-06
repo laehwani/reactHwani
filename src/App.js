a1532979029c5c4cf701b0d3b9e0a265023e980c
hwani <klh481@naver.com>
2023-11-06 14:40:03 +0900

    react 배열을 jsx 에서 사용하기 제거버튼 만들어보기,

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
-import { Button, Input, List, ListItem } from "@chakra-ui/react";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
+
+function UnorededList() {
+  return null;
+}
 
 function App(props) {
-  const [coffees, setCoffees] = useState(["라떼"]);
-  const [text, setText] = useState("");
+  const [foods, setFoods] = useState([]);
 
-  function 버튼클릭() {
-    const newCoffees = [...coffees];
-    newCoffees.push(text);
-    setCoffees(newCoffees);
-    console.log(coffees);
-    console.log(newCoffees);
+  function handleButtonClick(food) {
+    setFoods([...foods, food]);
+  }
+
+  function handleRemoveButtonClick(index) {
+    // console.log(index + "번 째 아이템 지우기");
+    // const nextFoods = [...foods];
+    // nextFoods.splice(index, 1);
+
+    // const nextFoods = foods.filter((item, i) => i != index);
+    // setFoods(nextFoods);
+
+    setFoods(foods.filter((item, i) => i != index));
   }
 
   return (
     <div>
-      <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
-      <Button onClick={버튼클릭}>추가버튼</Button>
-
-      <List>
-        {coffees.map((c, i) => (
-          <ListItem key={i}>{c}</ListItem>
-        ))}
-      </List>
+      <Button onClick={() => handleButtonClick("커피")}>커피</Button>
+      <Button onClick={() => handleButtonClick("케잌")}>케잌</Button>
+      <Button onClick={() => handleButtonClick("아이스")}>아이스</Button>
+      <Box>
+        <UnorderedList>
+          {foods.map((item, index) => (
+            <ListItem key={index}>
+              {item}{" "}
+              <Button onClick={() => handleRemoveButtonClick(index)}>
+                지우기
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
     </div>
   );
 }
