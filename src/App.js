290bb3f74adde20e2a16d15bb6048d7da5f39206
hwani <klh481@naver.com>
2023-11-06 11:38:21 +0900

    react 객체타입은 ... 으로 복사해서 써야함.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Box, Input, Text } from "@chakra-ui/react";
+
+function App(props) {
+  const [메세지, set메세지] = useState("");
+  const [obj, setObj] = useState({ 메세지: "" });
+
+  function 객체메세지변경(e) {
+    // const newobj = {...obj};
+    // newobj.메세지 = e.target.value;
+    // setObj(newobj);
+    setObj({ ...obj, 메세지: e.target.value });
+  }
+
+  return (
+    <div>
+      <Box>
+        <Input
+          value={메세지}
+          onChange={(e) => set메세지(e.target.value)}
+        ></Input>
+        <Text>{메세지}</Text>
+      </Box>
+      <Box>
+        <Input value={obj.메세지} onChange={객체메세지변경}></Input>
+        <Text>{obj.메세지}</Text>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
