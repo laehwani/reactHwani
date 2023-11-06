d799864076b2530453c24eb719038805d044ae98
hwani <klh481@naver.com>
2023-11-06 17:30:45 +0900

    react immer 연습문제 개여러움 수정중

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { useImmer } from "use-immer";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
 
+let nextId = 1;
 function App(props) {
-  return <div></div>;
+  const [items, updateItems] = useImmer([]);
+
+  function handleBtnClick(todo) {
+    updateItems((draft) => {
+      draft.push({ id: nextId++, done: false, text: todo });
+    });
+  }
+
+  function handleDoneBtn(id) {
+    updateItems((draft) => {
+      const item = draft.find((item) => item.id == id);
+      if (item) {
+        item.done = true;
+      }
+    });
+  }
+
+  return (
+    <div>
+      <Button onClick={() => handleBtnClick("자바공부")}>자바공부</Button>
+      <Button onClick={() => handleBtnClick("점심먹기")}>점심먹기</Button>
+      <Button onClick={() => handleBtnClick("잠자기")}>잠자기</Button>
+
+      <Box>
+        <UnorderedList>
+          {items.map((item) => (
+            <ListItem
+              key={item.id}
+              textDecoration={item.done ? "line-through" : "none"}
+            >
+              {item.text}
+              <Button onClick={() => handleDoneBtn(item.id)}>완료</Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
