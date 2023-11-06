dc75bbdb4268de7a1aed36eda138d4494dc210e9
hwani <klh481@naver.com>
2023-11-06 15:29:54 +0900

    react 배열을 jsx 에서 사용하기 복습필요

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Box, Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+
+function App(props) {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function 버튼클릭() {
+    setItems([...items, text]);
+  }
+  function 제거버튼(index) {
+    // const nextItems = [...items];
+    // nextItems.splice(index, 1);
+    // setItems(nextItems);
+    setItems(items.filter((item, i) => i != index));
+  }
+
+  return (
+    <div>
+      <Box>
+        <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
+        <Button onClick={버튼클릭}>추가버튼</Button>
+      </Box>
+      <Box>
+        <UnorderedList>
+          {items.map((item, i) => (
+            <ListItem key={i}>
+              {item}
+              <Button onClick={() => 제거버튼(i)}>제거</Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
