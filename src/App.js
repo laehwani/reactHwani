3a4ca1ff3ae866917b0c4c8cfa5e91fc9a2a5468
hwani <klh481@naver.com>
2023-11-08 09:30:09 +0900

    react useEffect의 두번째 파라미터는 첫번째파라미터(setup, effect) 를 실행시키는 트리거가 되는 값

--- src/App.js
+++ src/App.js
@@ -1,43 +1,23 @@
-import { Button, Text } from "@chakra-ui/react";
-import { createContext, useContext, useState } from "react";
-
-function CComp() {
-  // 3. context 사용하기 : useContext(Context);
-
-  const message = useContext(MessageContext);
-  return <Text>{message}</Text>;
-}
-
-function BComp() {
-  return <CComp />;
-}
-
-function Acomp() {
-  return <BComp />;
-}
+import React, { useEffect, useState } from "react";
+import { Button, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState("");
 
-  // message state를 Ccomp에 전달 하기
-  // 1. context 만들기 : createContext();
-  // 2. context에 state 넣기 :
-  // <...Context.Provider value={...}></Context.Provider>
-  // 3. tree안에서 context 사용 하기
-
+  // 첫번째 파라미터 : The function with your Effect's logic = 부작용이 있는 함수
+  // 두번째 파라미터 : 첫째 파라미터를 실행시키는 값의 배열
+  //                만약, 빈 배열이면 첫 랜더링 때만 실행됨
+  useEffect(() => {
+    console.log("effect 함수 실행됨");
+  }, [number, text]);
   return (
     <div>
-      <Button onClick={() => setMessage("바꾼 메세지")}>메시지 바꾸기</Button>
-      <MessageContext.Provider value={message}>
-        <Acomp />
-      </MessageContext.Provider>
+      <Button onClick={() => setNumber(number + 1)}>버튼</Button>
+      <Text>{number}</Text>
+      <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
     </div>
   );
 }
-
-// 1. Context 만들기
-// context 이름은 대문자로 시작하고 ...Context로 지어 주는게 관습이다
-// 보통 디폴트 값으로 null을 많이 넣어준다
-const MessageContext = createContext(null);
 
 export default App;
