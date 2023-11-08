e7ee8432bb60815b98cdd306648d12fba5e31b65
hwani <klh481@naver.com>
2023-11-08 14:01:31 +0900

    react useEffect 사용시 주의해야할 점!!

--- src/App.js
+++ src/App.js
@@ -1,55 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { Box, Select, Spinner, Text } from "@chakra-ui/react";
-import axios from "axios";
 
 function App(props) {
-  const [customerId, setCustomerId] = useState(0);
-  const [customer, setCustomer] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
+  const [number, setNumber] = useState(0);
 
+  // useEffect 사용시 자주 하는 실수
   useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get("/api/main1/sub4?id=" + customerId)
-      .then((response) => response.data)
-      .then((data) => setCustomer(data))
-      .catch((error) => setCustomer(null))
-      .finally(() => setIsLoading(false));
-  }, [customerId]);
-
-  return (
-    <div>
-      <Select
-        placeholder="고객 번호를 선택하세요"
-        onChange={(e) => setCustomerId(e.target.value)}
-      >
-        {/*option[value=$]{$}*10*/}
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        <option value="7">7</option>
-        <option value="8">8</option>
-        <option value="9">9</option>
-        <option value="10">10</option>
-      </Select>
-      <Box>
-        {isLoading && <Spinner />}
-
-        {isLoading || (
-          <>
-            {customer === null ? (
-              <Text>조회한 고객이 없습니다. 다른 번호를 선택해주세요.</Text>
-            ) : (
-              <Text>고객 이름 : {customer.name}</Text>
-            )}
-          </>
-        )}
-      </Box>
-    </div>
-  );
+    console.log("코드 실행");
+    // setNumber(number + 1);
+    // 위의 코드처럼 절대 trigger 하는 값을 변경하면 안된다 주의!
+  }, [number]);
+  return <div></div>;
 }
 
 export default App;
